eb67e6795650f4b4fdf67fd5b971ae8cad926c47
Aravind-MA <102525547+Aravind-MA@users.noreply.github.com>
2023-02-24 18:47:47 +0530

    Player

--- VR_Character.h
+++ VR_Character.h
@@ -42,7 +42,7 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
-	FORCEINLINE FHitResult GetHitFire()const { return HitFire; }
+	
 
 
 private:
